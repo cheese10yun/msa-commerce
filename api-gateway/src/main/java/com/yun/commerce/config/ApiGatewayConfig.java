5f3c0c64bc2e72550cb5a5d6592054fc03b02d27
Yun <cheese10yun@gmail.com>
2019-06-03 23:22:52 +0900

    Add module api-gateway

---
+++ api-gateway/src/main/java/com/yun/commerce/config/ApiGatewayConfig.java
@@ -0,0 +1,8 @@
+package com.yun.commerce.config;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+public class ApiGatewayConfig {
+
+}
---
+++ api-gateway/src/main/java/com/yun/commerce/config/DiscoveryClientConfig.java
@@ -0,0 +1,8 @@
+package com.yun.commerce.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+public class DiscoveryClientConfig {
+
+}
