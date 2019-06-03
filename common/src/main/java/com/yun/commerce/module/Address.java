c65e283dd642b3248c81f7ba5c6a8ed9b2d5ae75
Yun <cheese10yun@gmail.com>
2019-06-03 23:39:08 +0900

    공통 모듈 정리

---
+++ common/src/main/java/com/yun/commerce/module/Address.java
@@ -0,0 +1,30 @@
+package com.yun.commerce.module;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Address {
+
+  @Column(name = "address_street")
+  private String street;
+
+  @Column(name = "address_detail")
+  private String detail;
+
+  @Column(name = "address_zip_code")
+  private String zipCode;
+
+  @Builder
+  public Address(final String street, final String detail, final String zipCode) {
+    this.street = street;
+    this.detail = detail;
+    this.zipCode = zipCode;
+  }
+}
---
+++ common/src/test/java/com/yun/commerce/module/EmailTest.java
@@ -0,0 +1,48 @@
+package com.yun.commerce.module;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmailTest {
+
+  @Test
+  public void Email_test() {
+    final String value = "cheese10yun@gmail.com";
+    final Email email = Email.of(value);
+    Assert.assertThat(email.getValue(), is(value));
+    Assert.assertThat(email.getId(), is("cheese10yun"));
+    Assert.assertThat(email.getHost(), is("gmail.com"));
+  }
+
+  @Test
+  public void Email_Invalidation() {
+    final String value = "cheese1gmail.com";
+    final Email email = Email.of(value);
+    Assert.assertThat(email.getValue(), is(value));
+    Assert.assertThat(email.getHost(), is(nullValue()));
+    Assert.assertThat(email.getId(), is(nullValue()));
+  }
+
+  @Test
+  public void Email_Invalidation_test() {
+    final String value = "chees@e1gmailm";
+    final Email email = Email.of(value);
+    Assert.assertThat(email.getValue(), is(value));
+    Assert.assertThat(email.getId(), is("chees"));
+    Assert.assertThat(email.getHost(), is("e1gmailm"));
+  }
+
+  @Test
+  public void Email_Invalidation_test_01() {
+    final String value = "@asd.com";
+    final Email email = Email.of(value);
+    Assert.assertThat(email.getValue(), is(value));
+    Assert.assertThat(email.getId(), is(""));
+    Assert.assertThat(email.getHost(), is("asd.com"));
+  }
+
+}
