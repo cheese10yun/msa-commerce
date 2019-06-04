58d78084344d88a378311319f1d29f3048af8741
Yun <cheese10yun@gmail.com>
2019-06-05 00:11:09 +0900

    패키지 정리

--- api-gateway/src/main/java/com/yun/commerce/config/ApiGatewayConfig.java
+++
@@ -1,8 +0,0 @@
-//package com.yun.commerce.config;
-//
-//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-//
-//@EnableZuulProxy
-//public class ApiGatewayConfig {
-//
-//}
--- api-gateway/src/main/java/com/yun/commerce/config/DiscoveryClientConfig.java
+++
@@ -1,8 +0,0 @@
-//package com.yun.commerce.config;
-//
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//
-//@EnableDiscoveryClient
-//public class DiscoveryClientConfig {
-//
-//}
