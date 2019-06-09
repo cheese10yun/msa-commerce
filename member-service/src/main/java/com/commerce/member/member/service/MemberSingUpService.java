95c5e3e7b70cd9354eb49a310fc3b1bb816df050
Yun <cheese10yun@gmail.com>
2019-06-10 01:54:58 +0900

    notification 추가

--- member-service/src/main/java/com/commerce/member/member/service/MemberSingUpService.java
+++ member-service/src/main/java/com/commerce/member/member/service/MemberSingUpService.java
@@ -1,5 +1,6 @@
 package com.commerce.member.member.service;
 
+import com.commerce.member.infra.noti.MemberSignUp;
 import com.commerce.member.member.dao.MemberRepository;
 import com.commerce.member.member.domain.Member;
 import com.commerce.member.member.dto.MemberSignUpRequest;
@@ -14,10 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberSingUpService {
 
   private final MemberRepository memberRepository;
+  private final MemberSignUp memberSignup;
 
   @Transactional
   public Member signUp(final MemberSignUpRequest dto) {
-    return memberRepository.save(dto.toEntity());
+    final Member member = memberRepository.save(dto.toEntity());
+
+    final boolean send = memberSignup.send(member);
+
+    return member;
   }
 
 }
---
+++ notification-service/src/main/java/com/commerce/notification/MemberSignupListener.java
@@ -0,0 +1,18 @@
+//package com.commerce.notification;
+//
+//import com.commerce.notification.domain.Member;
+//import org.springframework.cloud.stream.annotation.StreamListener;
+//import org.springframework.cloud.stream.messaging.Processor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+////@Component
+//public class MemberSignupListener {
+//
+//  @Bean
+//  @StreamListener(Processor.INPUT)
+//  public void receiveSignUp(final Member member){
+//
+//  }
+//
+//}
